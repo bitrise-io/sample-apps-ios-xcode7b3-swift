5af8c33fad675b4a145dd016bd1feb7f8d00131e
Viktor Benei <viktor.benei@gmail.com>
2015-07-13 12:35:13 +0200

    init

---
+++ sample-apps-ios-xcode7b3-swift/sample-apps-ios-xcode7b3-swiftTests/sample_apps_ios_xcode7b3_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  sample_apps_ios_xcode7b3_swiftTests.swift
+//  sample-apps-ios-xcode7b3-swiftTests
+//
+//  Created by Viktor Benei on 7/13/15.
+//  Copyright © 2015 Bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import git_github_com_bitrise_io_sample_apps_ios_xcode7b3_swift
+
+class sample_apps_ios_xcode7b3_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
